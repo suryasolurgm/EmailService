78330af49a37e3c6f2982b32cd0ff98a9e0713c5
Surya Solur G M <suryasolurgm007@gmail.com>
2024-09-11 17:08:56 +0530

    after refactor

--- src/main/java/dev/surya/emailservice/EmailService.java
+++ src/main/java/dev/surya/emailservice/EmailService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmailServiceMwfEveApplication {
+public class EmailService {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmailServiceMwfEveApplication.class, args);
+        SpringApplication.run(EmailService.class, args);
     }
 
 }
